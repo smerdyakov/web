bc5c8fca820fa68bf90778b55a0af47b782afb0e
Kirk Hendricks <kirk.e.hendricks@gmail.com>
2019-09-11 04:06:57 -0700

    Added package-lock.json file. Use this with npm to run from same state after git pull

--- public/login.js
+++ public/login.js
@@ -1,9 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-/*
-Edit this file, then run browserify logincode.js > login.js
-browserify is a npm package that allows me to access the npm package blakejs (a one-way hashing function) on the client-side.
-Kirk 11Sep2019
-*/
 
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
