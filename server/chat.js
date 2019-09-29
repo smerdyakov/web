6e9d592f8160c41427ed43f3d34ebbdadf52d3d2
zeumai <55117050+zeumai@users.noreply.github.com>
2019-09-29 16:08:24 -0600

    Made minor changes to account for differences in underlying functions and storage mechanisms

--- server/chat.js
+++ server/chat.js
@@ -35,7 +35,7 @@ function Client(wsocket, request) {
   this.wsocket = wsocket;
   this.id = Utils.getCookieID(request);
   this.send = (message) => {
-    wsocket.send(JSON.stringify(message)); 
+    wsocket.send(JSON.stringify(message));
   };
   this.open = () => {
     return wsocket.readyState == WebSocket.OPEN;
@@ -53,14 +53,16 @@ function Chatroom(id) {
 
   this.add = (client) => {
     this.clients[client.id] = client;
-    const loggedMessages = Utils.loggedMessages(this.id);
-    loggedMessages.forEach( message => { client.send(message);});
-    this.broadcast(`${Utils.usernameOf(client.id)} entered the chat.`);
+    Utils.loggedMessages(this.id).then(loggedMessages => {
+      loggedMessages.forEach( message => {
+        client.send(JSON.parse(message['Message']));});
+      this.broadcast(`${Utils.usernameOf(client.id)} entered the chat.`);
 
-    client.broadcast = (message) => {
-      message = makeMessage(Utils.usernameOf(client.id), message.text, message.time);
-      broadcastHelper(this, message);
-    };
+      client.broadcast = (message) => {
+        message = makeMessage(Utils.usernameOf(client.id), message.text, message.time);
+        broadcastHelper(this, message);
+      };
+    });
   };
 }
 
