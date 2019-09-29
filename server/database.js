634a2bfea314351a2c8ec212f3f4f3925c16bce5
zeumai <55117050+zeumai@users.noreply.github.com>
2019-09-29 16:09:27 -0600

    A chatroom will now only be added if it doesn't yet exist

--- server/database.js
+++ server/database.js
@@ -85,7 +85,7 @@ function chatroomExists(chatroomID) {
 function insertChatroom(chatroomID) {
 //Adds a chatroom to the database.
   var sql =
-    'INSERT INTO tbl_chatrooms (Chatroom) ' +
+    'INSERT IGNORE INTO tbl_chatrooms (Chatroom) ' +
     'VALUES (?);';
   var con = mysql.createConnection(dbConfig);
 
@@ -97,12 +97,67 @@ function insertChatroom(chatroomID) {
   });
 }
 
+function getInternalChatroomID(chatroomID) {
+//Given a chatroom ID, returns the database's internal ID for that chatroom.
+  var sql =
+    'SELECT Chatroom_ID ' +
+    'FROM tbl_chatrooms ' +
+    'WHERE Chatroom = ?;';
+  var con = mysql.createConnection(dbConfig);
+  var record;
+  var internalID;
+
+  return new Promise(function(resolve, reject) {
+    con.query(sql, chatroomID, function(err, results) {
+      if (err) return reject(err);
+      record = results[0];
+      if (record) internalID = record['Chatroom_ID'];
+      resolve(internalID);
+    });
+  });
+}
+
+function insertMessage(internalChatroomID, message) {
+//Adds a message (and its associated chatroom, specified by internal ID) to the database.
+  var sql =
+    'INSERT INTO tbl_messages (Chatroom_ID, Message) ' +
+    'VALUES (?, ?);';
+  var con = mysql.createConnection(dbConfig);
+
+  return new Promise(function(resolve, reject) {
+    con.query(sql, [internalChatroomID, JSON.stringify(message)], function(err, results, fields) {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+}
+
+function getChatroomMessages(internalChatroomID) {
+//Returns the messages associated with a chatroom.
+  var sql =
+    'SELECT Message ' +
+    'FROM tbl_messages ' +
+    'WHERE Chatroom_ID = ?;';
+  var con = mysql.createConnection(dbConfig);
+  var internalID;
+
+  return new Promise(function(resolve, reject) {
+    con.query(sql, internalChatroomID, function(err, results) {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+}
+
 Database = {
   userExists,
   insertUser,
   getHashedPassword,
   chatroomExists,
   insertChatroom,
+  getInternalChatroomID,
+  insertMessage,
+  getChatroomMessages,
 }
 
 module.exports = Database;
